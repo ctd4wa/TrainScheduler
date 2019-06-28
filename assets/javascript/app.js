ab4efef9ca361511073fa5c4ac91b5ad6de88b60
Chris Duong <ctd4wa@virginia.edu>
2019-06-28 00:16:25 -0400

    javascript almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,33 +11,53 @@ var firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-//Variables
+
 var database = firebase.database();
 
-//Train information gets stored and displayed onto webpage
-    //Get 'snapshot' of stored data at initial load and subsequent value changes
-    database.ref().on("child_added", function(snapshot) {
-        var transportation = snapshot.val().transportation;
-    	var destination = snapshot.val().destination;
-    	var firstTransport = snapshot.val().firsttransport;
-        var frequency = snapshot.val().frequency;
+$("#add-btn").on("click", function() {
+    event.preventDefault();
+    
+    // Grabbing user input
+    var transport = $("#transport-input").val().trim();
+    var destination = $("#destination-input").val().trim();
+    var time = $("#time-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
 
-        console.log(destination);
-        console.log(firstTransport);
+    console.log(transport);
+
+    // Uploading data into database
+    database.ref().push({
+        transportation: transport,
+        destination: destination,
+        firsttransport: time,
+        frequency: frequency,
+    });
+});
+
+// Snapshot for grabbing input
+database.ref().on("child_added", function(snapshot) {
+    console.log(snapshot.val());
+    var transportation = snapshot.val().transportation;
+    var destination = snapshot.val().destination;
+    var firstTransport = snapshot.val().firsttransport;
+    var frequency = snapshot.val().frequency;
+
+    console.log(destination);
+    console.log(firstTransport);
         
-        var firstTimeConverted = moment(firstTransport, "hh:mm").subtract(1, "years");
-        console.log(firstTimeConverted);
-        var currentTime = moment();
-        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-        console.log("DIFFERENCE IN TIME: " + diffTime);
-        var tRemainder = diffTime % frequency;
-        console.log(tRemainder);
-        var tMinutesTillTrain = frequency - tRemainder;
-        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-        nextTrain = moment(nextTrain).format("hh:mm");
-        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    var firstTimeConverted = moment(firstTransport, "hh:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+    var tRemainder = diffTime % frequency;
+    console.log(tRemainder);
+    var tMinutesTillTrain = frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    nextTrain = moment(nextTrain).format("hh:mm");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
 
     $("#tbody").append("<tr> <td>" + transportation + "</td><td>" + destination + "</td><td>" + frequency + " mins</td><td>" + nextTrain + "</td><td>" + tMinutesTillTrain+ " mins</td>");
@@ -45,33 +65,4 @@ var database = firebase.database();
     }, function(errorObject) {
         console.log("The read failed: " + errorObject.code)
     });
-
-//User submits train information
-    $("#add-btn").on("click", function() {
-        event.preventDefault();
         
-        var transport = $("#transport-input").val().trim();
-        var destination = $("#destination-input").val().trim();
-        var time = $("#time-input").val().trim();
-        var frequency = $("#frequency-input").val().trim();
-        
-        console.log(transport);
-
-        //Adds info into database
-        database.ref().push({
-            transportation: transport,
-            destination: destination,
-            firsttransport: time,
-            frequency: frequency,
-        });
-    });
-
-
-//User clears train information    
-    $("#clear-btn").on("click", function() {
-        //create a reference to the file to delete
-        var databaseRef = database.ref();
-
-        databaseRef.remove();
-        $("#tbody").empty();
-    });
